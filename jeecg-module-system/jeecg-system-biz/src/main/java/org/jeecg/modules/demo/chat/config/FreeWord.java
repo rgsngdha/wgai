63067c41c2ea6ae90fbf0c2cee6473dc4b714720
root <1552138571@qq.com>
2025-02-25 13:50:52 +0800

    add  重构包名

--- jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/demo/chat/config/FreeWord.java
+++
@@ -1,5 +0,0 @@
-package org.jeecg.modules.demo.chat.config;
-
-public class FreeWord {
-    public static final String[] keyWord = new String[]{"白吃", "白食","免费","打折","打个折","便宜","折"};
-}
--- jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/tab/chart/AIchart.java
+++
@@ -1,9 +0,0 @@
-package org.jeecg.modules.tab.chart;
-
-/**
- * @author Administrator
- * @date 2024/3/27 13:51
- */
-public class AIchart {
-
-}
